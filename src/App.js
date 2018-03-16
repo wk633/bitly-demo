ec62e8aeca57d51d46d6a39d524a90c7e866b007
wk633 <wk633@outlook.com>
2018-03-16 15:40:23 -0700

    finish display part II

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import background from './background-image.png';
 import logo from './logo.svg';
+import clickIcon from './click-icon.svg';
 import './App.css';
 
 class App extends Component {
@@ -21,9 +22,61 @@ class App extends Component {
           <div className="App-sub-slogan">
             Join Bitly, the world's leading link management platform.
           </div>
-          <div className="App-input">
-            <input className="input-area" placeholder="Paste a link to shorten it"/>
-            <div className="short-btn">SHORTEN</div>
+          <div>
+            <div className="App-input">
+              <input className="input-area" placeholder="Paste a link to shorten it"/>
+              <div className="short-btn">SHORTEN</div>
+            </div>
+            <div className="App-result-display">
+              <div className="result-wrapper">
+              
+                <div className="result-element">
+                  <div className="first-line">
+                    Bulletproof Coffee: Debunking the Hot Buttered Hype
+                  </div>
+                  <div className="second-line">
+                    http:// gizmodo.com/xxxxx
+                  </div>
+                  <div className="last-line">
+                    <a>bit.ly/19iOLs6B3</a>
+                    <div className="stat-num-icon">
+                      <div className="stat-num">34,908</div><img className="click-icon" src={clickIcon}></img>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="result-element">
+                  <div className="first-line">
+                    Bulletproof Coffee: Debunking the Hot Buttered Hype
+                  </div>
+                  <div className="second-line">
+                    http:// gizmodo.com/xxxxx
+                  </div>
+                  <div className="last-line">
+                    <a>bit.ly/19iOLs6B3</a>
+                    <div className="stat-num-icon">
+                      <div className="stat-num">34,908</div><img className="click-icon" src={clickIcon}></img>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="result-element">
+                  <div className="first-line">
+                    Bulletproof Coffee: Debunking the Hot Buttered Hype
+                  </div>
+                  <div className="second-line">
+                    http:// gizmodo.com/xxxxx
+                  </div>
+                  <div className="last-line">
+                    <a>bit.ly/19iOLs6B3</a>
+                    <div className="stat-num-icon">
+                      <div className="stat-num">34,908</div><img className="click-icon" src={clickIcon}></img>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
       </div>
