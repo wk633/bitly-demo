7bb24fac7563d9631a1d1556226e69545dce4f32
wk633 <wk633@outlook.com>
2018-03-16 14:33:18 -0700

    basic framework style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import background from './background-image.png';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header" style={{backgroundImage: `url(${background})`}}>
+          <nav className="App-nav">
+            <img src={logo} className="App-logo"></img>
+            <div className="App-link">TOUR</div>
+            <div className="App-link">ENTERPRISE</div>
+            <div className="App-link">RESOURCES</div>
+            <div className="App-link">ABOUT</div>
+          </nav>
+          <div className="App-slogan">
+            SHORTEN. SHARE. MEASURE.
+          </div>
+          <div className="App-sub-slogan">
+            Join Bitly, the world's leading link management platform.
+          </div>
+          <div className="App-input">
+            <input className="input-area" placeholder="Paste a link to shorten it"/>
+            <div className="short-btn">SHORTEN</div>
+          </div>
+        </div>
       </div>
     );
   }
