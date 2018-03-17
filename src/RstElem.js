d16425eec7e0846e1d69a6ba9d9c33bc50a94d80
wk633 <wk633@outlook.com>
2018-03-16 18:32:43 -0700

    display style fix

--- src/RstElem.js
+++ src/RstElem.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import clickIcon from './click-icon.svg';
 class RstElem extends Component{
+    trans(num){
+        return (num+"").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    }
     render(){
+        const hashcode = this.props.info.shortUrl.split("/")[3];
+        const globalCount = this.trans(this.props.info.globalCount);
         return (
             <div className="result-element">
                 <div className="first-line">
@@ -11,9 +16,9 @@ class RstElem extends Component{
                 {this.props.info.longUrl}
                 </div>
                 <div className="last-line">
-                <a target="_blank" href={this.props.info.shortUrl}>{this.props.info.shortUrl}</a>
+                <a target="_blank" href={this.props.info.shortUrl}>bitly/<span style={{fontWeight: "normal"}}>{hashcode}</span></a>
                 <div className="stat-num-icon">
-                    <div className="stat-num">{this.props.info.globalCount}</div><img alt="click-icon" className="click-icon" src={clickIcon}></img>
+                    <div className="stat-num">{globalCount}</div><img alt="click-icon" className="click-icon" src={clickIcon}></img>
                 </div>
                 </div>
             </div>
